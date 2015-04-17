58a6bed5a55b6a5854fd18f0ee5bdaea8c5fb975
Marvin Muuß <marvinmuuss@gmx.de>
2015-04-17 20:37:41 +0200

    Bewegung der Monster unabhaengig bzw einzigartig

--- Game/Koopa.swift
+++ Game/Koopa.swift
@@ -20,6 +20,41 @@ class Koopa:Monster {
       fatalError("init(coder:) has not been implemented")
   }
   
+  override func update(delta: NSTimeInterval){
+    var gravity = CGPointMake(0.0, -450.0)
+    var gravityStep = gravity * CGFloat(delta)
+    var forwardMove = CGPointMake(400.0, 0.0)
+    var forwardStep = forwardMove * CGFloat(delta)
+    
+    self.velocity = self.velocity + gravityStep
+    
+    self.velocity = CGPointMake(self.velocity.x * 0.9, self.velocity.y)
+    
+    var jumpHeight = CGPointMake(0, 310)
+    var restrictJump:CGFloat = 150
+    
+    if (self.mightJump && self.onGround) {
+      self.velocity = self.velocity + jumpHeight
+    } else if (!self.mightJump && self.velocity.y > restrictJump) {
+      self.velocity = CGPointMake(self.velocity.x, restrictJump)
+    }
+    
+    if (self.moveForward){
+      self.velocity = self.velocity + forwardStep
+    }
+    
+    var minMovement = CGPointMake(0.0,-450)
+    var maxMovement = CGPointMake(120,250)
+    
+    self.velocity = CGPointMake(clamp(self.velocity.x, min: minMovement.x, max: maxMovement.x), clamp(self.velocity.y, min: minMovement.y, max: maxMovement.y))
+    
+    var velocityStep = self.velocity * CGFloat(delta)
+    
+    self.desiredPosition = self.position + velocityStep
+    
+    super.update(delta)
+  }
+  
 /*  var velocity: CGPoint
   var desiredPosition: CGPoint
   var moveForward: Bool
--- MonsterInterface.swift
+++ MonsterInterface.swift
@@ -31,36 +31,6 @@ class Monster:SKSpriteNode {
   }
   
   func update (delta: NSTimeInterval){
-    var gravity = CGPointMake(0.0, -450.0)
-    var gravityStep = gravity * CGFloat(delta)
-    var forwardMove = CGPointMake(400.0, 0.0)
-    var forwardStep = forwardMove * CGFloat(delta)
-    
-    self.velocity = self.velocity + gravityStep
-    
-    self.velocity = CGPointMake(self.velocity.x * 0.9, self.velocity.y)
-    
-    var jumpHeight = CGPointMake(0, 310)
-    var restrictJump:CGFloat = 150
-    
-    if (self.mightJump && self.onGround) {
-      self.velocity = self.velocity + jumpHeight
-    } else if (!self.mightJump && self.velocity.y > restrictJump) {
-      self.velocity = CGPointMake(self.velocity.x, restrictJump)
-    }
-    
-    if (self.moveForward){
-      self.velocity = self.velocity + forwardStep
-    }
-    
-    var minMovement = CGPointMake(0.0,-450)
-    var maxMovement = CGPointMake(120,250)
-    
-    self.velocity = CGPointMake(clamp(self.velocity.x, min: minMovement.x, max: maxMovement.x), clamp(self.velocity.y, min: minMovement.y, max: maxMovement.y))
-    
-    var velocityStep = self.velocity * CGFloat(delta)
-    
-    self.desiredPosition = self.position + velocityStep
     
     checkForAndResolveCollisions(self, forLayer: tileMap.layerNamed("walls"))
   }
